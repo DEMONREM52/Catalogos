08a714d2aa82e4b7a59a6beb9cdbed4f3364668d
DEMONREM52 <javiergarciamontoya2003@gmail.com>
2024-05-17 20:45:31 -0500

    camboe las credenciales de la base de datos jyamayoristas

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 const DB_HOST = process.env.DB_HOST || "localhost";
 const DB_USER = process.env.DB_USER || "root";
-const DB_PASSWORD = process.env.DB_PASSWORD || "";
+const DB_PASSWORD = process.env.DB_PASSWORD || "fdfgdfgdgdfg";
 const DB_NAME = process.env.DB_NAME || "j&a";
 const DB_PORT = process.env.DB_PORT || "3306";
 
